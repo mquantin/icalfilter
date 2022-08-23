950271c05a3d02523cdc2fc50dfd04fcf277ff67
Matthieu Quantin <matthieu@tinta.fr>
2022-08-23 12:46:19 +0200

    with month day values options + log files for less print

--- icalfilter.py
+++ icalfilter.py
@@ -1,9 +1,17 @@
 import icalendar
 import datetime
 import pytz
+import logging
+
+logging.basicConfig(filename='filter.log',  level=logging.DEBUG)
+
 utc = pytz.utc
 
-YEAR = 2018
+YEAR = 2022
+MONTH = 9
+DAY = 1
+
+
 
 def main():
     with open('in.ics', 'r', encoding='utf-8') as f:
@@ -27,36 +35,42 @@ def main():
                     until_date = rrule['UNTIL'][0]
 
                     if type(until_date) == datetime.datetime:
-                        return until_date >= utc.localize(datetime.datetime(YEAR, 1, 1))
+                        return until_date >= utc.localize(datetime.datetime(YEAR, MONTH, DAY))
 
                     if type(until_date) == datetime.date:
-                        return until_date >= datetime.date(2019, 1, 1)
+                        return until_date >= datetime.date(YEAR, MONTH, DAY)
 
                     raise Exception('Unknown date format for "UNTIL" field')
 
             # not reccurrent
             if type(start_date) == datetime.datetime:
-                return start_date >= utc.localize(datetime.datetime(YEAR, 1, 1))
+                return start_date >= utc.localize(datetime.datetime(YEAR, MONTH, DAY))
 
             if type(start_date) == datetime.date:
-                return start_date >= datetime.date(2019, 1, 1)
+                return start_date >= datetime.date(YEAR, MONTH, DAY)
 
             raise Exception('ARGH')
 
 
+        includedCnt = 0
+        excludedCnt = 0
         for item in cal.subcomponents:
             if item.name == 'VEVENT':
                 start_date = item['dtstart'].dt
                 if active_event(item):
-                    print ('INCLUDE', item['summary'], repr(start_date))
+                    includedCnt +=1
+                    logging.info(str('INCLUDE ' + repr(start_date) + ' ' + item['summary']))
                     outcal.add_component(item)
                 else:
-                    print ('EXCLUDE', item['summary'], repr(start_date))
+                    excludedCnt +=1
+                    logging.info(str('EXCLUDE ' + repr(start_date) + ' ' + item['summary']))
                     pass
             else:
+                logging.info('SPECIAL_INCLUDE', item)
                 outcal.add_component(item)
 
         with open('out.ics', 'wb') as outf:
             outf.write(outcal.to_ical(sorted=False))
+        print ('Included events: ', includedCnt,'\nExcluded events: ', excludedCnt)
 
 main()
